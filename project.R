525887169e172886e0f5cb4e20f0fa4ecb90a16a
bhargob30 <56303284+bhargob30@users.noreply.github.com>
2019-10-12 18:25:38 +0530

    Update project.R

--- project.R
+++ project.R
@@ -4,21 +4,16 @@ project_data$volact
 lm.r<-lm(project_data$volact~project_data$race+project_data$fire+project_data$theft+project_data$age+project_data$involact+project_data$income,data=project_data)
 summary(lm.r)
 plot(lm.r)
-project_data_WO <- project_data[c(-24, -7),] 
-model2 = lm(project_data_WO$volact ~ 1 + project_data_WO$race + project_data_WO$fire +
-              project_data_WO$theft + project_data_WO$age + project_data_WO$involact + 
-              project_data_WO$income, data = project_data_WO)
-plot(model2)
 project_data$volact
 residlm.r<-resid(lm.r)
 residlm.r
 fittedlm.r<-fitted(lm.r)
 fittedlm.r
+# observed vs fitted plot<- our model is good
 plot(fittedlm.r,project_data$volact,ylab="fitted",xlab="observed")
 abline(a=0,b=1,col="purple")
 plot(project_data$volact)
 lines(fittedlm.r,col="green")
-RSS(lm.r)
 summary(lm.r)
 ?lm
 ?summary(lm)
@@ -26,11 +21,13 @@ RSE<-1.928
 df<-40
 RSS<-(RSE^2)*df
 RSS
+#acf and pacf plot<- there is no indiaction of dependence among the residuals
 acf(residlm.r,lag.max=NULL,type="correlation",plot=TRUE) 
-warnings()
 pacf(residlm.r,lag.max=NULL,plot=TRUE)
+#plotting square of the residuals vs fitted values<- indiactes that variance function is an increasing function of the mean
 residsqlm.r<-1:47
 for(i in 1:47){residsqlm.r[i]<-residlm.r[i]*residlm.r[i]}
 residsqlm.r
 plot(fittedlm.r,residsqlm.r,ylab="residual sqaured",xlab="fitted")
+#installing rmarkdown
 install.packages("rmarkdown")
